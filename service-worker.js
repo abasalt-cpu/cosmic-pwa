42cf51adc26bc704468a8b0a74e1c456a3a3149e
abasalt-cpu <abasalt@gmail.com>
2026-09-13 14:32:43 +0330

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cosmic-number-v1';
+const CACHE_NAME = 'cosmic-number-v2';
 const ASSETS = ['./','./index.html','./manifest.json','./data/data.js','./data/tables.js',
 './js/cosmic_logic.js','./js/natal_chart.js','./js/zodiac.js','./js/content_modules.js','./js/baby_name.js','./js/app.js',
 './icons/icon-192.png','./icons/icon-512.png'];
@@ -9,5 +9,12 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))).then(() => self.clients.claim()));
 });
 self.addEventListener('fetch', (event) => {
-  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).catch(() => cached)));
+  if(event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request).then((fresh) => {
+      const copy = fresh.clone();
+      caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+      return fresh;
+    }).catch(() => caches.match(event.request))
+  );
 });
